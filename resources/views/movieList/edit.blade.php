953499b2ebf2f366032d751ba5c1175b6fcffd29
ryu <ryuta.lacrosse.soccer@gmail.com>
2024-06-18 21:31:00 +0900

    ラジオボタンの追加・調整

--- resources/views/movieList/edit.blade.php
+++ resources/views/movieList/edit.blade.php
@@ -40,9 +40,29 @@
                             </label>
                             <label class="w-full max-w-3xl mx-auto">
                                 <p>鑑賞方法</p>
-                                <input
+                                <!-- <input
                                     class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-4 pl-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
-                                    type="text" name="how_to_watch" value="{{ $List->how_to_watch }}" />
+                                    type="text" name="how_to_watch" value="{{ $List->how_to_watch }}" /> -->
+                                <div class="flex flex-direction-reverse justify-center form-group mb-4">
+                                    <div class="form-check form-check-inline is-invalid mx-1">
+                                        <input class="form-check-input"
+                                            id="" type="radio" name="how_to_watch">
+                                            <label class="text-slate-950 text-base"
+                                                for="1">映画館</label>
+                                    </div>
+                                    <div class="form-check form-check-inline is-invalid mx-1">
+                                        <input class="form-check-input"
+                                            id="2" type="radio" name="how_to_watch">
+                                            <label class="text-slate-950 text-base"
+                                                for="2">アプリ</label>
+                                    </div>
+                                    <div class="form-check form-check-inline is-invalid mx-1">
+                                        <input class="form-check-input"
+                                            id="3" type="radio" name="how_to_watch">
+                                            <label class="text-slate-950 text-base"
+                                                for="3">レンタル</label>
+                                    </div>
+                                </div>
                             </label>
                             <label class="w-full max-w-3xl mx-auto">
                                 <p>一言メモ</p>
@@ -63,31 +83,37 @@
                                     class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-4 pl-4 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
                                     type="text" name="evaluations" value="{{ $List->evaluations }}" />
                             </label> -->
-                            <div>
-                                <div>
-                                    <div class="flex flex-direction-reverse justify-center">
-                                        <input class="hidden"
-                                            id="1" type="radio" name="evaluations">
-                                            <label class="text-gray-500 text-2xl cursor-pointer hover:text-yellow-300 transition-colors"
-                                                for="1">★</label>
-                                        <input class="hidden"
-                                            id="2" type="radio" name="evaluations">
-                                            <label class="text-gray-500 text-2xl cursor-pointer hover:text-yellow-300 transition-colors"
-                                                for="2">★</label>
-                                        <input class="hidden"
-                                            id="3" type="radio" name="evaluations">
-                                            <label class="text-gray-500 text-2xl cursor-pointer hover:text-yellow-300 transition-colors"
-                                                for="3">★</label>
-                                        <input class="hidden"
-                                            id="4" type="radio" name="evaluations">
-                                            <label class="text-gray-500 text-2xl cursor-pointer hover:text-yellow-300 transition-colors"
-                                                for="4">★</label>
-                                        <input class="hidden"
-                                            id="5" type="radio" name="evaluations">
-                                            <label class="text-gray-500 text-2xl cursor-pointer hover:text-yellow-300 transition-colors"
-                                                for="5">★</label>
-                                    </div>
+                            <div class="flex flex-direction-reverse justify-center form-group mb-4">
+                                <div class="form-check form-check-inline is-invalid mx-1">
+                                    <input class="hidden form-check-input"
+                                        id="review1" type="radio" name="evaluations" value="1" {{ old ('review1') == '1' ? 'checked' : ''}} checked>
+                                        <label class="form-check-label text-gray-500 text-2xl cursor-pointer hover:text-yellow-300 transition-colors"
+                                            for="review1">★</label>
                                 </div>
+                                <div class="form-check form-check-inline is-invalid mx-1">
+                                    <input class="hidden form-check-input"
+                                        id="review2" type="radio" name="evaluations" value="2" {{ old ('review2') == '2' ? 'checked' : ''}}>
+                                        <label class="form-check-label text-gray-500 text-2xl cursor-pointer hover:text-yellow-300 transition-colors"
+                                            for="review2">★</label>
+                                </div>
+                                <div class="form-check form-check-inline is-invalid mx-1">
+                                    <input class="hidden form-check-input"
+                                        id="review3" type="radio" name="evaluations" value="3" {{ old ('review3') == '3' ? 'checked' : ''}}>
+                                        <label class="form-check-label text-gray-500 text-2xl cursor-pointer hover:text-yellow-300 transition-colors"
+                                            for="review3">★</label>
+                                </div>
+                                <div class="form-check form-check-inline is-invalid mx-1">
+                                    <input class="hidden form-check-input"
+                                        id="review4" type="radio" name="evaluations" value="4" {{ old ('review4') == '4' ? 'checked' : ''}}>
+                                        <label class="form-check-label text-gray-500 text-2xl cursor-pointer hover:text-yellow-300 transition-colors"
+                                            for="review4">★</label>
+                                </div>
+                                <div class="form-check form-check-inline is-invalid mx-1">
+                                    <input class="hidden form-check-input"
+                                        id="review5" type="radio" name="evaluations" value="5" {{ old ('review5') == '5' ? 'checked' : ''}}>
+                                        <label class="form-check-label text-gray-500 text-2xl cursor-pointer hover:text-yellow-300 transition-colors"
+                                            for="review5">★</label>
+                                    </div>
                             </div>
 
                             <div class="mt-8 w-full flex items-center justify-center gap-10">
